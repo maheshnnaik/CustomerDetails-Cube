74f49a84d57deec3e3a242c700e972b3544c6bdd
maheshnnaik <maheshnaik2747@gmail.com>
2024-08-16 10:19:33 +0530

    Add constant values

---
+++ src/constant.ts
@@ -0,0 +1,109 @@
+export const PICSUMURL = "https://picsum.photos/v2/list";
+
+export const customers = [
+    {
+        id: 0,
+        name: "Customer 1",
+        title: "Customer Title",
+        address: "Customer Address",
+        details: "Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups."
+    },
+    {
+        id: 1,
+        name: "Customer 2",
+        title: "Customer Title",
+        address: "Customer Address",
+        details: "Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups."
+    },
+    {
+        id: 2,
+        name: "Customer 3",
+        title: "Customer Title",
+        address: "Customer Address",
+        details: "Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups."
+    },
+    {
+        id: 3,
+        name: "Customer 4",
+        title: "Customer Title",
+        address: "Customer Address",
+        details: "Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups."
+    },
+    {
+        id: 4,
+        name: "Customer 5",
+        title: "Customer Title",
+        address: "Customer Address",
+        details: "Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups."
+    },
+    {
+        id: 5,
+        name: "Customer 6",
+        title: "Customer Title",
+        address: "Customer Address",
+        details: "Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups."
+    },
+    {
+        id: 6,
+        name: "Customer 7",
+        title: "Customer Title",
+        address: "Customer Address",
+        details: "Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups."
+    },
+    {
+        id: 7,
+        name: "Customer 8",
+        title: "Customer Title",
+        address: "Customer Address",
+        details: "Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups."
+    },
+    {
+        id: 8,
+        name: "Customer 9",
+        title: "Customer Title",
+        address: "Customer Address",
+        details: "Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups."
+    },
+    {
+        id: 9,
+        name: "Customer 10",
+        title: "Customer Title",
+        address: "Customer Address",
+        details: "Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups."
+    },
+    {
+        id: 10,
+        name: "Customer 11",
+        title: "Customer Title",
+        address: "Customer Address",
+        details: "Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups."
+    },
+    {
+        id: 11,
+        name: "Customer 12",
+        title: "Customer Title",
+        address: "Customer Address",
+        details: "Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups."
+    },
+    {
+        id: 12,
+        name: "Customer 13",
+        title: "Customer Title",
+        address: "Customer Address",
+        details: "Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups."
+    },
+    {
+        id: 13,
+        name: "Customer 14",
+        title: "Customer Title",
+        address: "Customer Address",
+        details: "Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups."
+    },
+    {
+        id: 14,
+        name: "Customer 15",
+        title: "Customer Title",
+        address: "Customer Address",
+        details: "Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups."
+    },
+]
